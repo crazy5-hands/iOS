81ffb139209e94802075dad56ac4654ee49cf3dc
KO-YAMAKO <ko.foot1103@gmail.com>
2018-01-22 16:57:52 +0900

    line login button , 色のクラスの作成, ログインボタンクラスの作成

---
+++ hands/hands/LoginButton.swift
@@ -0,0 +1,21 @@
+//
+//  LoginButton.swift
+//  hands
+//
+//  Created by 山浦功 on 2018/01/22.
+//  Copyright © 2018年 KoYamaura. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+
+//Login button
+class LoginButton: UIButton{
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesBegan(touches, with: event)
+        self.backgroundColor = .white
+    }
+}
+
