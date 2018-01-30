02806f4334c356f159c2d4c6d40ba3e7d4637734
KO-YAMAKO <ko.foot1103@gmail.com>
2018-01-30 20:14:21 +0900

    set storyboard and class

---
+++ hands/hands/ProfileTableViewCell.swift
@@ -0,0 +1,30 @@
+//
+//  ProfileTableViewCell.swift
+//  hands
+//
+//  Created by 山浦功 on 2018/01/30.
+//  Copyright © 2018年 KoYamaura. All rights reserved.
+//
+
+import UIKit
+
+class ProfileTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var profileImageView: UIImageView!
+    @IBOutlet weak var userNameLabel: UILabel!
+    @IBOutlet weak var sumOfWillJoinLabel: UILabel!
+    @IBOutlet weak var sumOfJoinedLabel: UILabel!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
