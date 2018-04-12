4c659297be13f3a775d733eb6b2a6e2cef7039f2
KO-YAMAKO <ko.foot1103@gmail.com>
2018-04-12 14:59:19 +0900

    #14 add strage model file

---
+++ hands/hands/Model/StorageModel.swift
@@ -0,0 +1,30 @@
+//
+//  StrageModel.swift
+//  hands
+//
+//  Created by 山浦功 on 2018/04/12.
+//  Copyright © 2018年 KoYamaura. All rights reserved.
+//
+
+import Foundation
+import Firebase
+
+
+class StorageModel {
+    
+    // 写真のアップロード
+    func uplodeImage(_ image: UIImage, url: String) -> URL? {
+        var downloadURL: URL?
+        let strageRef = Storage.storage().reference(forURL: "gs://hands-8efa5.appspot.com/") //参照先
+        let imageRef = strageRef.child(url)
+        let imageData = UIImageJPEGRepresentation(image, 1.0)
+        imageRef.putData(imageData!, metadata: nil){ metadata, error in
+            if (error != nil){
+                print("hello")
+            }else {
+                downloadURL = (metadata?.downloadURL())!
+            }
+        }
+        return downloadURL
+    }
+}
