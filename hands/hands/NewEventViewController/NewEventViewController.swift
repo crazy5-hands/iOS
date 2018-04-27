9793a065aa1f5233a9eed8db78592fa117affcbe
KO-YAMAKO <ko.foot1103@gmail.com>
2018-04-28 02:47:45 +0900

    #28 add group, swift and storyboard

---
+++ hands/hands/NewEventViewController/NewEventViewController.swift
@@ -0,0 +1,13 @@
+//
+//  NewEventViewController.swift
+//  hands
+//
+//  Created by 山浦功 on 2018/04/28.
+//  Copyright © 2018年 KoYamaura. All rights reserved.
+//
+
+import UIKit
+
+class NewEventViewController: UIViewController {
+
+}
