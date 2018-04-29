ece30e28f345778d5667c44f0fa89ff020c5811f
KO-YAMAKO <ko.foot1103@gmail.com>
2018-04-29 23:42:00 +0900

    #36 edit tableviewcell

--- hands/hands/UserTableViewCell/UserTableViewCell.swift
+++ hands/hands/UserTableViewCell/UserTableViewCell.swift
@@ -10,15 +10,19 @@ import UIKit
 
 class UserTableViewCell: UITableViewCell {
 
+    @IBOutlet weak var profileImageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
     }
     
+    func updateCell(image: UIImage, name: String){
+        self.profileImageView .image = image
+        self.nameLabel.text = name
+    }
 }
