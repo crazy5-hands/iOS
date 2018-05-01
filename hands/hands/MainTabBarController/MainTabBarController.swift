d2be36879b0b1facfd2b8ebb9f647dd9380a5c60
KO-YAMAKO <ko.foot1103@gmail.com>
2018-05-01 12:51:31 +0900

    #23 add user icon to project

--- hands/hands/MainTabBarController/MainTabBarController.swift
+++ hands/hands/MainTabBarController/MainTabBarController.swift
@@ -9,6 +9,9 @@
 import UIKit
 
 class MainTabBarController: UITabBarController {
+    
+    private let tabColor = UIColor(red: 0.0, green: 255, blue: 0.0, alpha: 1)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         var viewcontrollers = [UIViewController]()
@@ -18,15 +21,16 @@ class MainTabBarController: UITabBarController {
         viewcontrollers.append(listViewController!)
         
         let searchViewController = UIStoryboard(name: "SearchViewController", bundle: nil).instantiateInitialViewController()
-        searchViewController?.tabBarItem = UITabBarItem(tabBarSystemItem: .search, tag: 2)
+//        searchViewController?.tabBarItem = UITabBarItem(tabBarSystemItem: .search, tag: 2)
+        searchViewController?.tabBarItem = UITabBarItem(title: "検索", image: UIImage(named: "icon-search"), selectedImage: UIImage(named: "icon-search"))
         viewcontrollers.append(searchViewController!)
         
         let notificationViewController = UIStoryboard(name: "NotificationViewController", bundle: nil).instantiateInitialViewController()
-        notificationViewController?.tabBarItem = UITabBarItem(tabBarSystemItem: .featured, tag: 3)
+        notificationViewController?.tabBarItem = UITabBarItem(title: "通知", image: UIImage(named: "icon-notification"), selectedImage: UIImage(named: "icon-notification-selected"))
         viewcontrollers.append(notificationViewController!)
         
         let profileViewController = UIStoryboard(name: "ProfileViewController", bundle: nil).instantiateInitialViewController()
-        profileViewController?.tabBarItem = UITabBarItem(tabBarSystemItem: .contacts, tag: 4)
+        profileViewController?.tabBarItem = UITabBarItem(title: "プロフィール", image: UIImage(named: "icon-user"), tag: 3)
         viewcontrollers.append(profileViewController!)
         self.viewControllers = viewcontrollers.map{ UINavigationController(rootViewController: $0)}
         
