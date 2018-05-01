cd0ec950b2fb511c5ae20672f16e015d6183c360
KO-YAMAKO <ko.foot1103@gmail.com>
2018-05-01 17:00:42 +0900

    #23 add collectionview to storyboard and runnning is error

--- hands/hands/ProfileViewController/ProfileViewController.swift
+++ hands/hands/ProfileViewController/ProfileViewController.swift
@@ -7,29 +7,73 @@
 //
 
 import UIKit
+import Firebase
 
-class ProfileViewController: UIViewController {
+class ProfileViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
 
+    private let kSectionPhoto = 0
+    private let kSectionUsername = 1
+    private let kSectionNote = 2
+    private let kSectionOthers = 3
+    private var userRef: DatabaseReference!
+    private var uid = ""
+    private var user = User()
+    private var refHandle: DatabaseHandle?
+    
+    @IBOutlet private weak var collectionView: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        uid = (Auth.auth().currentUser?.uid)!
+        self.userRef = Database.database().reference().child("users").child(uid)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.userRef.observe(.value, with: { (snapshot) in
+            print(snapshot.value as! String)
+        }, withCancel: { (error) in
+            print(error.localizedDescription)
+        })
+//        self.refHandle = self.userRef.observe(DataEventType.value, with: { (snapshot) in
+//            let dict = snapshot.value(forKey: "username") as! String
+//            print(dict)
+////            self.user.
+//        })
     }
-    */
-
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        if let handle = self.refHandle {
+            self.userRef.removeObserver(withHandle: handle)
+        }
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        // write segue here
+    }
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 4
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        return UICollectionViewCell()
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        switch section {
+        case kSectionPhoto:
+            return 1
+        case kSectionUsername:
+            return 1
+        case kSectionNote:
+            return 1
+        case kSectionOthers:
+            return 4
+        default:
+            return 0
+        }
+    }
 }
