4fdd5f4001bacda56b3ea7576929243660108c5b
KO-YAMAKO <ko.foot1103@gmail.com>
2018-05-01 14:46:15 +0900

    #23 add list image and complete

--- hands/hands/MainTabBarController/MainTabBarController.swift
+++ hands/hands/MainTabBarController/MainTabBarController.swift
@@ -17,16 +17,16 @@ class MainTabBarController: UITabBarController {
         var viewcontrollers = [UIViewController]()
         
         let listViewController = UIStoryboard(name: "ListViewController", bundle: nil).instantiateInitialViewController()
-        listViewController?.tabBarItem = UITabBarItem(tabBarSystemItem: .bookmarks, tag: 1)
+        listViewController?.tabBarItem = UITabBarItem(title: "一覧", image: UIImage(named: "icon-list"), tag: 0)
         viewcontrollers.append(listViewController!)
         
         let searchViewController = UIStoryboard(name: "SearchViewController", bundle: nil).instantiateInitialViewController()
 //        searchViewController?.tabBarItem = UITabBarItem(tabBarSystemItem: .search, tag: 2)
-        searchViewController?.tabBarItem = UITabBarItem(title: "検索", image: UIImage(named: "icon-search"), selectedImage: UIImage(named: "icon-search"))
+        searchViewController?.tabBarItem = UITabBarItem(title: "検索", image: UIImage(named: "icon-search"), tag: 1)
         viewcontrollers.append(searchViewController!)
         
         let notificationViewController = UIStoryboard(name: "NotificationViewController", bundle: nil).instantiateInitialViewController()
-        notificationViewController?.tabBarItem = UITabBarItem(title: "通知", image: UIImage(named: "icon-notification"), selectedImage: UIImage(named: "icon-notification-selected"))
+        notificationViewController?.tabBarItem = UITabBarItem(title: "通知", image: UIImage(named: "icon-notification"), tag: 2)
         viewcontrollers.append(notificationViewController!)
         
         let profileViewController = UIStoryboard(name: "ProfileViewController", bundle: nil).instantiateInitialViewController()
