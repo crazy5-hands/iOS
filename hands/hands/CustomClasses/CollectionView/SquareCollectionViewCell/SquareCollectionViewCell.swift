13d8f1d95210be4d090b8d30b9efd579de56ef3b
KO-YAMAKO <ko.foot1103@gmail.com>
2018-05-03 13:52:23 +0900

    #21 add square collection view cell

---
+++ hands/hands/CustomClasses/CollectionView/SquareCollectionViewCell/SquareCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  SquareCollectionViewCell.swift
+//  hands
+//
+//  Created by 山浦功 on 2018/05/03.
+//  Copyright © 2018年 KoYamaura. All rights reserved.
+//
+
+import UIKit
+
+class SquareCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
