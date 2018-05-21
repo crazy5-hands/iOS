c7c5f93dd9c1ac98b671cc1e049d279232401d48
KO-YAMAKO <ko.foot1103@gmail.com>
2018-05-21 17:21:28 +0900

    #100 edit cool

--- hands/hands/Model/Cost.swift
+++ hands/hands/Model/Cost.swift
@@ -7,3 +7,35 @@
 //
 
 import Foundation
+
+struct Cost {
+    
+    var id: String
+    var created_at: NSDate
+    var event_id: String
+    var cost: Int
+    var dictionary: [String: Any] {
+        return [
+            "id": self.id,
+            "created_at": self.created_at,
+            "event_id": self.event_id,
+            "cost": self.cost,
+        ]
+    }
+    
+    init?(dictionary: [String: Any]) {
+        let id = dictionary["id"] as! String
+        let created_at = dictionary["created_at"] as! NSDate
+        let event_id = dictionary["title"] as! String
+        let cost = dictionary["body"] as! Int
+        
+        self.init(id: id, created_at: created_at, event_id: event_id, cost: cost)
+    }
+    
+    init(id: String, created_at: NSDate, event_id: String, cost: Int) {
+        self.id = id
+        self.created_at = created_at
+        self.event_id = event_id
+        self.cost = cost
+    }
+}
--- hands/hands/Utility/CostUtil.swift
+++ hands/hands/Utility/CostUtil.swift
@@ -7,3 +7,67 @@
 //
 
 import Foundation
+import Firebase
+
+class CostUtil {
+    
+    func create(newCost: Cost, complition: @escaping (Bool) -> Void) {
+        let db = Firestore.firestore()
+        db.collection("costs").addDocument(data: newCost.dictionary) { (error) in
+            if let error = error {
+                print(error.localizedDescription)
+                complition(false)
+            } else {
+                complition(true)
+            }
+        }
+    }
+    
+    func update(target: Cost, complition: @escaping (Bool) -> Void) {
+        let db = Firestore.firestore()
+        db.collection("costs").whereField("event_id", isEqualTo: target.event_id).getDocuments { (snapshot, error) in
+            if let snapshot = snapshot {
+                if snapshot.documents[0].exists == true {
+                    let documentID = snapshot.documents[0].documentID
+                    db.collection("costs").document(documentID).setData(target.dictionary, completion: { (error) in
+                        if let error = error {
+                            print(error.localizedDescription)
+                            complition(false)
+                        } else {
+                            complition(true)
+                        }
+                    })
+                } else {
+                    complition(false)
+                }
+                complition(false)
+            } else {
+                print(error!.localizedDescription)
+                complition(false)
+            }
+        }
+    }
+    
+    func destroy(target: Cost, complition: @escaping (Bool) -> Void) {
+        let db = Firestore.firestore()
+        db.collection("costs").whereField("event_id", isEqualTo: target.event_id).getDocuments { (snapshot, error) in
+            if let snapshot = snapshot {
+                if snapshot.documents[0].exists == true {
+                    let documentID = snapshot.documents[0].documentID
+                    db.collection("costs").document(documentID).delete(completion: { (error) in
+                        if let error = error {
+                            print(error.localizedDescription)
+                            complition(true)
+                        } else {
+                            complition(false)
+                        }
+                    })
+                } else {
+                    complition(false)
+                }
+            } else {
+                complition(false)
+            }
+        }
+    }
+}
