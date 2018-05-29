9081dd92ea8c6cf75c8c4462a8ea54b0a30faa69
KO-YAMAKO <ko.foot1103@gmail.com>
2018-05-29 21:35:55 +0900

    edit privacy policy

--- hands/hands/PrivacyPolicyViewController/PrivacyPolicyViewController.swift
+++ hands/hands/PrivacyPolicyViewController/PrivacyPolicyViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 import RxCocoa
 import RxSwift
 
-class PrivacyPolicyViewController: UIViewController {
+class PrivacyPolicyViewController: UIViewController, UIWebViewDelegate {
 
     @IBOutlet weak var webView: UIWebView!
     @IBOutlet weak var acceptPrivacyPolicyButton: UIButton!
@@ -18,11 +18,18 @@ class PrivacyPolicyViewController: UIViewController {
     private let viewModel = PrivacyPolicyViewModel()
     private let disposeBag = DisposeBag()
     private var agreed = false
+    private let activityIndicator = UIActivityIndicatorView()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        let requestURL = URL(fileURLWithPath: "https://crazy5-hands.github.io/hands.github.io/")
-        let request = URLRequest(url: requestURL)
+        self.activityIndicator.tintColor = .black
+        self.activityIndicator.frame = CGRect(x: 0, y: 0, width: 50, height: 50)
+        self.activityIndicator.center = self.view.center
+        self.activityIndicator.hidesWhenStopped = true
+        self.activityIndicator.activityIndicatorViewStyle = .gray
+        self.view.addSubview(self.activityIndicator)
+        let requestURL = URL(string: "https://crazy5-hands.github.io/hands.github.io/")
+        let request = URLRequest(url: requestURL!)
         self.webView.loadRequest(request)
     }
     
@@ -31,6 +38,25 @@ class PrivacyPolicyViewController: UIViewController {
         self.viewModel.acceptPrivacyPolocy
             .bind(to: self.nextButton.rx.isEnabled)
             .disposed(by: self.disposeBag)
+    }
+    
+    func webViewDidStartLoad(_ webView: UIWebView) {
+        self.activityIndicator.isHidden = false
+        self.activityIndicator.startAnimating()
+    }
+    
+    func webViewDidFinishLoad(_ webView: UIWebView) {
+        self.activityIndicator.stopAnimating()
+        self.activityIndicator.isHidden = true
+    }
+    
+    func webView(_ webView: UIWebView, didFailLoadWithError error: Error) {
+        self.activityIndicator.stopAnimating()
+        self.activityIndicator.isHidden = true
+    }
+    
+    @IBAction func dismiss(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
     }
     
     @IBAction func checkedAgree(_ sender: Any) {
