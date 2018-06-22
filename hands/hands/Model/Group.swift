ac8d8e8d6a94d2f10a65014a503ae25e6f9045a5
KO-YAMAKO <ko.foot1103@gmail.com>
2018-06-22 17:10:06 +0900

    #167 create model

---
+++ hands/hands/Model/Group.swift
@@ -0,0 +1,32 @@
+//
+//  Group.swift
+//  hands
+//
+//  Created by 山浦功 on 2018/06/22.
+//  Copyright © 2018年 KoYamaura. All rights reserved.
+//
+
+import Foundation
+
+struct Group {
+    var id: String
+    var createdAt: String
+    var name: String
+    var note: String
+    
+    var dictionary: [String: Any] {
+        return [
+            "id": self.id,
+            "created_at": self.createdAt,
+            "name": self.name,
+            "note": self.note
+        ]
+    }
+    
+    init?(dictionary: [String: Any]) {
+        self.id = dictionary["id"] as! String
+        self.createdAt = dictionary["created_at"] as! String
+        self.name = dictionary["name"] as! String
+        self.note = dictionary["note"] as! String
+    }
+}
