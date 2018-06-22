46b431a475849956286dd2412912f179a8a1a468
KO-YAMAKO <ko.foot1103@gmail.com>
2018-06-22 17:28:26 +0900

    #167 editing tableview status

--- hands/hands/GroupMemberTableViewController/GroupMemberTableViewController.swift
+++ hands/hands/GroupMemberTableViewController/GroupMemberTableViewController.swift
@@ -7,9 +7,33 @@
 //
 
 import UIKit
+import Firebase
 
 class GroupMemberTableViewController: UserListTableViewController {
     
+    let group: Group?
+    let tableViewStatus: TableViewStatus = .loading
+    
     override func getData() {
+        if let group = self.group {
+            let firestore = Firestore.firestore()
+            let groupRef = firestore.collection("groups")
+            groupRef.whereField("id", isEqualTo: group.id).getDocuments { (snapshot, error) in
+                if let snapshot = snapshot {
+                } else {
+                    print(error?.localizedDescription)
+                    self.tableViewStatus = .error
+                }
+            }
+        } else {
+            self.tableViewStatus = .error
+        }
     }
+}
+
+
+enum TableViewStatus {
+    case loading
+    case error
+    case success
 }
