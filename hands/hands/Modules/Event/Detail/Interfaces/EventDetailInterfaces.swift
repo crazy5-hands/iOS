dea089163f58449abf19943c6ddd33d7b20a7555
KO-YAMAKO <ko.foot1103@gmail.com>
2018-09-17 23:46:00 +0900

    added new EventDetail folders

---
+++ hands/hands/Modules/Event/Detail/Interfaces/EventDetailInterfaces.swift
@@ -0,0 +1,16 @@
+//
+//  EventDetailInterfaces.swift
+//  hands
+//
+//  Created by 山浦功 on 2018/09/17.
+//  Copyright © 2018年 KoYamaura. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+protocol EventDetailViewInterface: ViewInterface {
+}
+protocol EventDetailPresenterInterface: PresenterInterface {}
+protocol EventDetailWireframeInterface: WireframeInterface {}
+protocol EventDetailInteractorInterface: InteractorInterface {}
